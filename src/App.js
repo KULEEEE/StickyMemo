f2338b151e095c22b93b62bf3a4bda565f462ab8
heavencandle <whj0210@gmail.com>
2021-05-31 14:05:25 +0900

    [feature] Display memos as they are added

--- src/App.js
+++ src/App.js
@@ -4,17 +4,43 @@ import React, { useState } from 'react';
 import { v4 as uuid } from 'uuid';
 import { mockComponent } from 'react-dom/test-utils';
 import { __SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED } from 'react-dom';
+import { DefaultNote } from './DefaultNote';
 
-
-function DefaultNote({ addNote }) {
+// This TodoNote is mockup, which is jsut same as plain text
+function TodoNote() {
   return (
     <div className="single-note">
-      {/* ERROR */}
-      {/* <button className="add-button" onClick={addNote('todo')}>Make Todo</button>
-      <button className="add-button" onClick={addNote('plain')}>Make Plain</button> */}
-      {/* CORRECT */}
-      <button className="add-button" onClick={()=>{addNote('todo')}}>Make Todo</button>
-      <button className="add-button" onClick={()=>{addNote('plain')}}>Make Plain</button>
+      <div className="note-toolbar">
+        <button className='pin'><img src='' alt='pinIcon' /></button>
+        <button className='lang'><img src='' alt='langIcon' /></button>
+      </div>
+      <div className="plaintext">
+        <form>
+          <input className="content" type="text" placeholder="This is plain text note..."></input>
+        </form>
+      </div>
+      <div className="destroy">
+        <button className="destroy"><img src='' alt='destroyIcon' /></button>
+      </div>
+    </div>
+  );
+}
+
+function PlainNote() {
+  return (
+    <div className="single-note">
+      <div className="note-toolbar">
+        <button className='pin'><img src='' alt='pinIcon' /></button>
+        <button className='lang'><img src='' alt='langIcon' /></button>
+      </div>
+      <div className="plaintext">
+        <form>
+          <input className="content" type="text" placeholder="This is plain text note..."></input>
+        </form>
+      </div>
+      <div className="destroy">
+        <button className="destroy"><img src='' alt='destroyIcon' /></button>
+      </div>
     </div>
   );
 }
@@ -40,11 +66,23 @@ function App() {
 
       <section className="pinned-zone">
         <header>Pinned zone</header>
+        {pinned.map(pin => (
+          pin.notetype=='todo'? 
+          <TodoNote /> : 
+          <PlainNote />)
+          )
+        }
       </section>
 
       <section className="general-zone">
         <header>General zone</header>
         <DefaultNote addNote={addNote}/>
+        {general.map(singlenote => (
+          singlenote.notetype=='todo'? 
+            <TodoNote /> :
+            <PlainNote />)
+          )
+        }
       </section>
       
     </section>
