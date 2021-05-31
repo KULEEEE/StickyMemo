e09038bc6dfdda5bcc9bb38babf84be59df329d9
heavencandle <whj0210@gmail.com>
2021-05-31 12:30:09 +0900

    [feat] Add a single note to general zone

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import { v4 as uuid } from 'uuid';
+import { mockComponent } from 'react-dom/test-utils';
+import { __SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED } from 'react-dom';
 
-function App() {
+
+function DefaultNote({ addTodo, addPlain }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="single-note">
+      <button className="add-button" onClick={addTodo}>Make Todo</button>
+      <button className="add-button" onClick={addPlain}>Make Plain</button>
     </div>
   );
 }
 
+function App() {    
+  const [pinned, setPinned] = useState([]);
+  const [general, setGeneral] = useState([]);
+
+  function addTodo() {
+    const single_note = {
+      id: uuid(),
+      notetype: 'todo'
+    };
+    const newNotes = [...general, single_note];
+    setGeneral(newNotes);
+  }
+
+  function addPlain() {
+    const single_note = {
+      id: uuid(),
+      notetype: 'plain'
+    };
+    const newNotes = [...general, single_note];
+    setGeneral(newNotes);
+  }
+  
+  return (
+    <section className="App">
+      <header className="App-header"> 
+        <div>Project Name</div>
+      </header>
+
+      <section className="pinned-zone">
+        <header>Pinned zone</header>
+
+      </section>
+
+      <section className="general-zone">
+        <header>General zone</header>
+        <DefaultNote addPlain={addPlain} addTodo={addTodo} />
+      </section>
+      
+    </section>
+  );
+}
 export default App;
