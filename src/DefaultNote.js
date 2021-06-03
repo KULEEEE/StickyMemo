b2e671dc4802a8eba83f53b55e9bb3ee769f542e
ksj4654 <kksj4654@naver.com>
2021-06-03 18:18:18 +0900

    first commit

--- src/DefaultNote.js
+++ src/DefaultNote.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Button, ButtonToolbar } from 'react-bootstrap';
 
-export function DefaultNote({ addTodo, addPlain }) {
+export function DefaultNote({ addTodo, addPlain, addNote }) {
   return (
     <div className="default-note">
       {/* ERROR */}
